5799f01afcf84b712d518c0208b52aa320f7cf4e
Elena-Sea <smetyukelena@gmail.com>
2022-08-23 00:09:02 +0200

    modal in process

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,34 +5,47 @@ console.log(galleryItems);
 
 const galleryContainer = document.querySelector('.gallery');
 const galleryMarkup = createGalleryMarkup(galleryItems);
+
 galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
 
 function createGalleryMarkup(galleryItems) {
     return galleryItems.map(({ preview, original, description }) => {
         return `
-    <div class="gallery__item">
-        <a class="gallery__link" href="${original}">
-            <img
-                class="gallery__image"
-                src="${preview}"
-                data-source="${original}"
-                alt="${description}"
-            />
-        </a>
-    </div>
+     <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>   
     `
     }).join('');
 }
-
 galleryContainer.addEventListener("click", onGalleryContainerClick);
 
 function onGalleryContainerClick(e) {
-
+    // e.preventDefault();
     if (!e.target.classList.contains('gallery__image')) {
         return;
     }
-    console.log(234);
-
+    loadLargeImage(e.target.dataset.sourse) ;
 }
 
+function loadLargeImage(largeImageSrc) {
+    const largeImageModal = basicLightbox.create(`<img src="${largeImageSrc}" alt="${description}" width="1200" min-height="600"/>`,
+        {
+            onClose: () => { window.removeEventListener("keydown", onKeyboardEscape)}
+        });
+    largeImageModal.show();
+}
 
+// function onKeyboardEscape(e) {
+//     switch (e?.code) {
+//         case "Escape":
+//             closeModal();
+//             return;
+//     }
+// }
